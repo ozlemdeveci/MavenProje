53589fdce6e8311adf19c123fbdace1200a50cbd
ozlemdeveci <ozlemelisss@gmail.com>
2021-03-06 12:12:30 -0600

    3.commit

--- target/cucumber-default-reports/report.js
+++ target/cucumber-default-reports/report.js
@@ -1,90 +1,65 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/apiWorkflow.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/login.feature");
 formatter.feature({
-  "name": "Syntax HRMS API Workflow",
-  "description": "  Description:This feature file tests Syntax HRMS API WrokFlow",
+  "name": "Login Functionality",
+  "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@apiWorkflow"
+      "name": "@featureLeveltag"
     }
   ]
 });
-formatter.background({
-  "name": "",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.step({
-  "name": "a JWT is generated",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.hrms.Api.generateTokenSteps.a_JWT_is_generated()"
-});
-formatter.result({
-  "status": "skipped"
-});
 formatter.scenario({
-  "name": "Creating an Employee",
+  "name": "Login with Invalid credentials",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@apiWorkflow"
+      "name": "@featureLeveltag"
     },
     {
-      "name": "@createJson"
+      "name": "@smoke"
+    },
+    {
+      "name": "@regression"
     }
   ]
 });
-formatter.step({
-  "name": "a request is prepared to create an employee",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.hrms.Api.apiTestingFinalSteps.a_request_is_prepared_to_create_an_employee()"
-});
-formatter.result({
-  "status": "skipped"
+formatter.before({
+  "status": "passed"
 });
 formatter.step({
-  "name": "a POST call is made to create an Employee",
+  "name": "enter invalid credentials",
   "keyword": "When "
 });
 formatter.match({
-  "location": "com.hrms.Api.apiTestingFinalSteps.a_POST_call_is_made_to_create_an_Employee()"
+  "location": "com.hrms.stepdefinitions.LoginStepDefinition.enter_invalid_credentials()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
-  "name": "the status code for creating an employee is 201",
+  "name": "click on login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.hrms.stepdefinitions.LoginStepDefinition.click_on_login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify error msj",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "com.hrms.Api.apiTestingFinalSteps.the_status_code_for_creating_an_employee_is(int)"
+  "location": "com.hrms.stepdefinitions.LoginStepDefinition.verify_error_msj()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
-formatter.step({
-  "name": "the employee is created contains key \"Message\" and value \"Entry Created\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.hrms.Api.apiTestingFinalSteps.the_employee_is_created_contains_key_and_value(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "status": "skipped"
-});
-formatter.step({
-  "name": "the employeeID \"Employee[0].employee_id\" is stored in the global variable to be used for other calls",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.hrms.Api.apiTestingFinalSteps.the_employeeID_is_stored_in_the_global_variable_to_be_used_for_other_calls(java.lang.String)"
-});
-formatter.result({
-  "status": "skipped"
+formatter.embedding("image/png", "embedded0.png", "Login with Invalid credentials");
+formatter.after({
+  "status": "passed"
 });
 });
